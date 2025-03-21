a96a9fbedff87b6257d0af7b2f3ca99e3daa8723
tarek al azami <152443825+TAREK-AZM@users.noreply.github.com>
2025-03-21 14:26:09 +0000

    descover the functional interfaces and it's caractiristics

--- src/main/java/interface_Fonctionelle/IHello.java
+++ src/main/java/interface_Fonctionelle/IHello.java
@@ -1,6 +1,8 @@
 package interface_Fonctionelle;
 
 public interface IHello {
+    // the functional interface has only one abstract method
+    // the functional interface can has private, default methods implementation that used make internal logic
     void hello (String name);
     default void salut() {
         System.out.println("Aprendre Java!!!");
